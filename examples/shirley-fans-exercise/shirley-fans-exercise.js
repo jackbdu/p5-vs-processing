80d61217b953beb511afc814115f89b8f4e9ece7
rlwhuang <richard94922@gmail.com>
2015-02-09 01:25:55 +0800

    add shirley's example 1

---
+++ examples/shirley-fans-exercise/shirley-fans-exercise.js
@@ -0,0 +1,21 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+function setup() {
+	createCanvas(w, h);
+}
+
+function draw() {
+	smooth();
+	noStroke();
+	if (mouseIsPressed) {
+		if (frameCount % 10 == 0) {
+			fill(50 + frameCount * 3 % 255, 100, random(150, 255));
+			push();
+			translate(mouseX, mouseY);
+			rotate(radians(frameCount * 1 % 360));
+			rect(0, 0, 100, 10);
+			pop();
+		}
+	}
+}
