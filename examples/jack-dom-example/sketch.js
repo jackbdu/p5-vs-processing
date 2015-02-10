de7139474231d1c597d1cf24aa2f13499a9fb626
JackBDu <jd2804@nyu.edu>
2015-02-10 16:09:30 +0800

    basic p5.dom example created

---
+++ examples/jack-dom-example/sketch.js
@@ -0,0 +1,21 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+var creatElement;
+
+
+function setup() {
+	var myCanvas = createCanvas(w, h);
+	var text = createDiv("This is an HTML string! I will disappear if your mouseX goes to the right side!");
+	text.position(0, 0);
+	text.style("font-family:monospace; background-color:#FF0000; color:#FFFFFF; font-size:18pt; padding:10px;");
+}
+
+function draw() {
+	background(200);
+	fill(100);
+	rectMode(CENTER);
+	rect(mouseX, mouseY, 100, 100);
+	if (mouseX>w/2) {
+		removeElements();
+	}
+}
